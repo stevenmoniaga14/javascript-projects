86fedecd71500cbfcc089142773176d80207245b
Steven Moniaga <164908075+stevenmoniaga14@users.noreply.github.com>
2024-06-20 23:36:34 -0400

    finished exercise

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -8,39 +8,61 @@ strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 //2) 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
-	let check;
-	let output;
+	let check; strings[0];
+	let output = [];
 	//TODO: 2. write the code required for this step
 
+if (check.includes(',')) {
+		// split it to an array
+		// reverse the entries
+		// join the array into a new comma separated string
+		console.log(check);
+		output = check.split(',').reverse().join(',')
+	}
+
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
+	console.log(output);
 	return output;
+	
 }
+
+reverseCommas();
 
 //3)
 function semiDash() {
-	let check;
-	let output;
+	let check = strings[1];
+	let output = [];
 //TODO: write the code required for this step
-
-  
+	if(check.includes(';')) {
+		output = check.split(';').sort().join('-');
+	}
+	console.log(output);
 	return output;
 }
 
+semiDash();
+
 //4)
 function reverseSpaces() {
-	let check;
-	let output;
+	let check = strings[2];
+	let output = [];
   //TODO: write the code required for this step
-
-	return output;
+	if(check.includes(' ')){
+		output = (check.split(' ').sort().reverse().join(' '));
+	}	
+  return output;
 }
 
 //5)
 function commaSpace() {
-	let check;
+	let check = string [3];
 	let output;
 	//TODO: write the code required for this step
-  
+	if (check.includes(', ')) {
+		output = check.split(', ').reverse().join(',');
+	}
+	console.log(output);
+	
 	return output;
 }
 
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -4,8 +4,29 @@ let str = "LaunchCode";
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
+
+
+
 //Use a template literal to print the original and modified string in a descriptive phrase.
+
+console.log(`The original string is ${str}, the modified string is ${defaultString}`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
+let numLetters = Number(input.question("How many letters will be location?"));
+
+let newString = str.slice(numLetters) + str.slice(0, numLetters);
+let defaultString = str.slice(3) + str.slice(0, 3);
+
+
+console.log(newString);
+
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
+if (numLetters > str.length) {
+    console.log('Input Exceed length of the word.');
+    let defaultString = str.slice(3) + str.slice(0, 3);
+    console.log(`'We change ${str} into ${defaultString} using string methods'`);
+} else {
+    console.log(`'We change ${str} into ${newString} using string methods'`);
+}
