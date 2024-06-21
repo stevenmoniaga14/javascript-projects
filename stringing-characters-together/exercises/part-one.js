02fbb07089a52800f7a5362efdb88921b57806a7
Steven Moniaga <164908075+stevenmoniaga14@users.noreply.github.com>
2024-06-20 23:31:43 -0400

    finished exercise

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,10 +1,27 @@
 let num = 1001;
+let num_length = num.toString();
+
+
+let decimalValue = 123.45;
+let decimalLength = decimalValue.toString();
+
 
 //Returns 'undefined'.
 console.log(num.length);
 
+
 //Use type conversion to print the length (number of digits) of an integer.
+
+console.log(num_length.length);
+
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
 
+console.log(decimalLength.length - 1);
+
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+
+if (String(num).includes(".")) {
+    console.log(String(num).length-1);
+} else 
+    console.log(String(num).length);
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -4,14 +4,41 @@ let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
 
+let slice1 = language.slice(0,1);
+let slice2 = language.slice(4,5);
+let printJS = slice1.concat(slice2);
+console.log(printJS);
+
+
 //2. Without using slice(), use method chaining to accomplish the same thing.
+
+let language2 = language.split(',').toString();
+let result2 = language2[0] + language2[4];
+console.log(result2);
+
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
 
+console.log(`The abbreviation for ${language} is ${printJS}`);
+
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
+
+let quoteDay = "Believe you can and you're halfway there";
+console.log(quoteDay.toUpperCase().toLowerCase().split(""));
 
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+let newCase = notTitleCase.split("");
+
+newCase.splice(0, 1, "T");
+newCase.splice(-4, 1, "C");
+
+
+
+let result = newCase.join("");
+console.log(result);
+
+
