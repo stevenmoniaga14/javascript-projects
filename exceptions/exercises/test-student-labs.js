60088c69f58790d2bc7a623861ef645ce485a6ef
Steven Moniaga <164908075+stevenmoniaga14@users.noreply.github.com>
2024-07-20 16:40:50 -0400

    good

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,8 +1,15 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+    let result;
+    try {
+      result = lab.runLab(3);
+    } catch (e) {
+      result = "Error thrown";
+    }
+    console.log(`${lab.student} code worked: ${result === 27 ? true : result}`);
+    // This is saying that result === 27, the question mark is asking "IF result is 27 return true" 
+    // The : is the other option where if it does not return 27 then go to result
   }
 }
 
@@ -22,3 +29,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
