ca02a4c16b80a29c4c6d1c4c27905233540dadeb
Steven Moniaga <164908075+stevenmoniaga14@users.noreply.github.com>
2024-08-07 22:34:48 -0400

    completed

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,27 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+
+    missionAbort.addEventListener('mousemove', () => {
+        missionAbort.style.backgroundColor = "red";
+        // document.querySelector('#abortMission').style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener('mouseleave', () => {
+        missionAbort.style.backgroundColor = "";
+        // document.querySelector('#abortMission').style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener('click', function (){
+        window.confirm("Are you sure you want to abort the mission?");
+        if (missionAbort) {
+            paragraph.textContent = "Mission aborted! Space shuttle returning home"
+        }
+    })
+
+    button.addEventListener('click', function (){
+        paragraph.textContent = "Houston! We have a liftoff!"
+    });
     
 }
 
